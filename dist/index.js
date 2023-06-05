c67de28df7facceb1ee59ec3969e4aab5fe7f99d
Aleksandr <121160960+Aleksandr-MMM@users.noreply.github.com>
2023-06-05 18:50:35 +0300

    update test

--- dist/index.js
+++ dist/index.js
@@ -5,13 +5,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3003;
+const port = 3001;
+let students = [
+    {
+        id: 1,
+        name: 'Andrei'
+    },
+    {
+        id: 2,
+        name: 'Aleksander'
+    },
+    {
+        id: 3,
+        name: 'Vica'
+    },
+];
 app.get('/', (req, res) => {
     const a = 5;
     if (a > 6) {
         res.send(`a равна ${a}`);
     }
     res.send('Hello World!!!!');
+});
+app.get('/students', (req, res) => {
+    res.send(students);
 });
 app.listen(port, () => {
     console.log(`My App start to port: ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import express from 'express'
 
 const app = express()
-const port = 3003
+const port = 3001
 
+let students =[
+    {
+        id: 1,
+        name:'Andrei'
+    },
+    {
+        id: 2,
+        name:'Aleksander'
+    },
+    {
+        id: 3,
+        name:'Vica'
+    },
+]
 app.get('/', (req, res) => {
     const a=5
     if(a>6){
@@ -11,6 +25,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!!!!')
 })
 
+app.get('/students', (req, res) => {
+
+    res.send(students)
+})
 app.listen(port, () => {
     console.log(`My App start to port: ${port}`)
 })
